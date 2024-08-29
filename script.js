2e09a94db36cc076c162d94820c3424572f40a94
Lucas Sorge <lucasorge13@gmail.com>
2024-08-29 12:18:37 -0500

    progress bar

--- script.js
+++ script.js
@@ -4,9 +4,3 @@ function toggleMenu(){
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-const toggleButton = document.getElementById('theme-toggle');
-toggleButton.addEventListener('click', () => {
-  document.documentElement.toggleAttribute('data-theme');
-  toggleButton.textContent = document.documentElement.hasAttribute('data-theme') ? '☀️' : '🌙';
-});
